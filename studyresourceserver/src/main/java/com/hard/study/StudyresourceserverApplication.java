355810f20ac4c3764dbe3c1073f115570cec5ce6
Sumin <Sumin@DESKTOP-O7UD9KM>
2019-11-06 13:29:22 +0900

    resource server

---
+++ studyresourceserver/src/main/java/com/hard/study/StudyresourceserverApplication.java
@@ -0,0 +1,13 @@
+package com.hard.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyresourceserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyresourceserverApplication.class, args);
+	}
+
+}
